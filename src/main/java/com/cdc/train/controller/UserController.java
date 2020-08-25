61c56c81d2d1896920a99439e90143b8c7a290c7
ben-qiu-zou <1192690984@qq.com>
2020-08-25 14:09:12 +0800

    更新了日志和数据源的配置

--- src/main/java/com/cdc/train/controller/UserController.java
+++ src/main/java/com/cdc/train/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.cdc.train.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(value = "/train")
 public class UserController {
 
+    @GetMapping(value = "/login")
+    public String login() {
+        return "login";
+    }
 
 }
