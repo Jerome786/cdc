b5213caf1da89bbb84971ffe8f7286e7ada9ac29
wujiongbiao <1771452788@qq.com>
2020-09-13 19:37:12 +0800

    用户模块

---
+++ src/main/java/com/cdc/train/entity/dto/UserDTO.java
@@ -0,0 +1,42 @@
+package com.cdc.train.entity.dto;
+
+import com.cdc.train.entity.User;
+
+public class UserDTO extends User {
+    private String realName;
+    private String gender;
+    private String roleId;
+    private String roleName;
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
