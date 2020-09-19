1b10863bab8a02b1c4c75c08f21be06af8285242
wujiongbiao <1771452788@qq.com>
2020-09-19 10:20:08 +0800

    文章发布以及评论

---
+++ src/main/java/com/cdc/train/entity/dto/CommentDTO.java
@@ -0,0 +1,54 @@
+package com.cdc.train.entity.dto;
+
+import com.cdc.train.entity.Comment;
+
+public class CommentDTO extends Comment {
+    private String avatar;
+    private String nickname;
+
+    private String pUserId;
+
+    private String pNickname;
+
+    private String pcontent;
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getpUserId() {
+        return pUserId;
+    }
+
+    public void setpUserId(String pUserId) {
+        this.pUserId = pUserId;
+    }
+
+    public String getpNickname() {
+        return pNickname;
+    }
+
+    public void setpNickname(String pNickname) {
+        this.pNickname = pNickname;
+    }
+
+    public String getPcontent() {
+        return pcontent;
+    }
+
+    public void setPcontent(String pcontent) {
+        this.pcontent = pcontent;
+    }
+}
